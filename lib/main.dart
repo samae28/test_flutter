4b8bf69068e06474c78f9b2d1660f2fd303cb396
sarah <mortilsarahmae@gmail.com>
2024-08-19 07:04:44 +0800

    Flutter test trial

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white,
+                Colors.blueAccent,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
